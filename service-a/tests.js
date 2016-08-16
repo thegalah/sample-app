f09bd3bd9d543ffeb6a7ae443c08392aeb49a8d3
Stephen Provine <stephpr@microsoft.com>
2016-08-16 16:13:35 -0700

    Update flaky test.

--- service-a/tests.js
+++ service-a/tests.js
@@ -1,8 +1,11 @@
 var page = require("webpage").create();
-page.open("http://service-a", function (status) {
-    if (status !== "success") {
-        console.log("Status:" + status);
-        phantom.exit(1);
-    }
-    phantom.exit();
-})
+setTimeout(function () {
+    page.open("http://service-a", function (status) {
+        if (status !== "success") {
+            console.log("Status:" + status);
+            phantom.exit(1);
+        }
+        console.log("TESTS PASSED");
+        phantom.exit();
+    });
+}, 2000);
