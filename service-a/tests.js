7ae2ae961d6f0a3313d5c9383891a72a166337cb
Stephen Provine <stephpr@microsoft.com>
2016-08-16 12:46:32 -0700

    Update tests.

--- service-a/tests.js
+++ service-a/tests.js
@@ -1,5 +1,8 @@
 var page = require("webpage").create();
 page.open("http://service-a", function (status) {
-    console.log("Status: " + status);
+    if (status !== "success") {
+        console.log("Status:" + status);
+        phantom.exit(1);
+    }
     phantom.exit();
 })
